333b2043dcf4a3b2ea4bafb9b695f4bf46a4c81a
Keony Schröer <keony@edu.univali.br>
2020-12-05 10:32:00 -0300

    refactor: move giphy url to a constant

--- infra/remote/gif_repository.go
+++ infra/remote/gif_repository.go
@@ -6,12 +6,16 @@ import (
 	"net/url"
 )
 
+const giphyURL = "http://api.giphy.com/v1/gifs/search"
+
+// GifRepository is the struct used to fetch data from giphy api
 type GifRepository struct{}
 
+// Find is the implementation of protocol defined in data layer of GifRepository
 func (g GifRepository) Find(title string) (string, error) {
 	qt := url.QueryEscape(title)
 
-	resp, err := http.Get(fmt.Sprintf("http://api.giphy.com/v1/gifs/search?q=%v&api_key=s9GY9cDxXGHLCVCZifd0lh0rP8x1o2lh", qt))
+	resp, err := http.Get(fmt.Sprintf("%vq=%v&api_key=s9GY9cDxXGHLCVCZifd0lh0rP8x1o2lh", giphyURL, qt))
 
 	if err != nil {
 		return "", err
