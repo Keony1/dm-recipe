79ef4aac21a0751363f3db714c6b2cab44afd4cb
Keony Schröer <keony@edu.univali.br>
2020-12-04 23:35:01 -0300

    refactor: add types to receive the response from puppy api

--- data/protocols/puppy_respository.go
+++ data/protocols/puppy_respository.go
@@ -5,7 +5,17 @@ type PuppyRepository interface {
 	Load(search string) ([]PuppyResult, error)
 }
 
-// PuppyResult is the result from puppy api
+// PuppyResponse is the slice of results returned from puppy api
+type PuppyResponse struct {
+	Results []PuppyRecipe
+}
+
+//PuppyRecipe is the actual recipe returned from puppy api
+type PuppyRecipe struct {
+	Title, Href, Ingredients string
+}
+
+// PuppyResult is the parsed []PuppyResponse
 type PuppyResult struct {
 	Title       string
 	Href        string
