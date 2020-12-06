99ea819ccf789316e1de64e0fac22d3b6051f030
Keony Schröer <keony@edu.univali.br>
2020-12-06 11:08:29 -0300

    feat: ensure gifrepository will return if giphy api returns

--- infra/remote/gif_repository.go
+++ infra/remote/gif_repository.go
@@ -15,27 +15,27 @@ type GifRepository struct{}
 func (g GifRepository) Find(title string) (string, error) {
 	qt := url.QueryEscape(title)
 
-	resp, err := http.Get(fmt.Sprintf("%vq=%v&api_key=s9GY9cDxXGHLCVCZifd0lh0rP8x1o2lh", giphyURL, qt))
+	resp, err := http.Get(fmt.Sprintf("%v?q=%v&api_key=s9GY9cDxXGHLCVCZifd0lh0rP8x1o2lh", giphyURL, qt))
 
 	if err != nil {
 		return "", err
 	}
 
 	var gr gifResponse
-	convertToJSON(gr, resp)
+	convertToJSON(&gr, resp)
 
-	if gr.data != nil {
-		gif := gr.data[0]
-		return gif.url, nil
+	if len(gr.Data) > 0 {
+		gif := gr.Data[0]
+		return gif.URL, nil
 	}
 
 	return "", nil
 }
 
 type gifResponse struct {
-	data []gif
+	Data []gif
 }
 
 type gif struct {
-	url string
+	URL string
 }
